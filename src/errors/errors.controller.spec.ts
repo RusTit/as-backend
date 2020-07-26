aebff15605a923fdd788a8e7d2afcd6a03c815f4
Russkih Anatoly <titanofdarkness@gmail.com>
2020-07-26 18:43:17 +1000

    Errors implementation.

--- src/errors/errors.controller.spec.ts
+++ src/errors/errors.controller.spec.ts
@@ -1,5 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ErrorsController } from './errors.controller';
+import { ErrorsService } from './errors.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Error } from './Error.entity';
+
+const mockRepository = {
+  async save(): Promise<void> {
+    return Promise.resolve();
+  },
+
+  async find(): Promise<Error[]> {
+    return [];
+  },
+};
 
 describe('Errors Controller', () => {
   let controller: ErrorsController;
@@ -7,6 +20,13 @@ describe('Errors Controller', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ErrorsController],
+      providers: [
+        ErrorsService,
+        {
+          provide: getRepositoryToken(Error),
+          useValue: mockRepository,
+        },
+      ],
     }).compile();
 
     controller = module.get<ErrorsController>(ErrorsController);
--- src/errors/errors.service.spec.ts
+++ src/errors/errors.service.spec.ts
@@ -1,12 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ErrorsService } from './errors.service';
+import { Error } from './Error.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+
+const mockRepository = {
+  async save(): Promise<void> {
+    return Promise.resolve();
+  },
+
+  async find(): Promise<Error[]> {
+    return [];
+  },
+};
 
 describe('ErrorsService', () => {
   let service: ErrorsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ErrorsService],
+      providers: [
+        ErrorsService,
+        {
+          provide: getRepositoryToken(Error),
+          useValue: mockRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<ErrorsService>(ErrorsService);
