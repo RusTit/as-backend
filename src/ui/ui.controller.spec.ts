272beb63064e3b3545edf58c7b07acd288af75d6
Russkih Anatoly <titanofdarkness@gmail.com>
2020-08-01 15:34:30 +1000

    Fix tests.

--- src/ui/ui.controller.spec.ts
+++ src/ui/ui.controller.spec.ts
@@ -1,12 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UiController } from './ui.controller';
+import { AuthnetModule } from '../authnet/authnet.module';
+import { TransactionsCreatedService } from '../transactions-created/transactions-created.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { TransactionCreatedEntity } from '../transactions-created/TransactionCreated.entity';
+import { mockRepository } from '../transactions-created/TransactionCreated.mock';
+import { UiService } from './ui.service';
 
 describe('Ui Controller', () => {
   let controller: UiController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [AuthnetModule],
       controllers: [UiController],
+      providers: [
+        TransactionsCreatedService,
+        {
+          provide: getRepositoryToken(TransactionCreatedEntity),
+          useValue: mockRepository,
+        },
+        UiService,
+      ],
     }).compile();
 
     controller = module.get<UiController>(UiController);
--- src/ui/ui.service.spec.ts
+++ src/ui/ui.service.spec.ts
@@ -1,12 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UiService } from './ui.service';
+import { mockRepository } from '../transactions-created/TransactionCreated.mock';
+import { AuthnetModule } from '../authnet/authnet.module';
+import { TransactionsCreatedService } from '../transactions-created/transactions-created.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { TransactionCreatedEntity } from '../transactions-created/TransactionCreated.entity';
 
 describe('UiService', () => {
   let service: UiService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UiService],
+      imports: [AuthnetModule],
+      providers: [
+        TransactionsCreatedService,
+        {
+          provide: getRepositoryToken(TransactionCreatedEntity),
+          useValue: mockRepository,
+        },
+        UiService,
+      ],
     }).compile();
 
     service = module.get<UiService>(UiService);
