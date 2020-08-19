cfa7a0cfc6047b07c18cfb08b521c0ced5e39853
Russkih Anatoly <titanofdarkness@gmail.com>
2020-08-19 18:10:44 +1000

    Fix test.

--- src/bigcomhook/bigcomhook.controller.spec.ts
+++ src/bigcomhook/bigcomhook.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BigcomhookController } from './bigcomhook.controller';
+import { BigcomhookService } from './bigcomhook.service';
 
 describe('Bigcomhook Controller', () => {
   let controller: BigcomhookController;
@@ -7,6 +8,7 @@ describe('Bigcomhook Controller', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [BigcomhookController],
+      providers: [BigcomhookService],
     }).compile();
 
     controller = module.get<BigcomhookController>(BigcomhookController);
