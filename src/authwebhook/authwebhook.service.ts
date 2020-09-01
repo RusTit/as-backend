a0320601596bf34ff05ece250c68976b6c76c630
Russkih Anatoly <titanofdarkness@gmail.com>
2020-09-01 12:23:12 +1000

    Stub.

--- src/authwebhook/authwebhook.service.ts
+++ src/authwebhook/authwebhook.service.ts
@@ -28,20 +28,24 @@ export class AuthwebhookService {
 
   async processRefundVoidedTransactions(payload: WebhookDto): Promise<void> {
     Logger.debug(`Processing refund/voided transactions`);
-    const transactionDetails = await this.authnetService.getTransactionsDetails(
-      payload.payload.id,
-    );
-    const orderNumber = transactionDetails.order.invoiceNumber;
-    await this.shipStationProxy.init();
-    const orders = await this.shipStationProxy.getListOrders({
-      orderNumber: `${orderNumber}`,
-      pageSize: `500`, // to avoid paging issues
-    });
-    await Promise.all(
-      orders.map(async (order) => {
-        const { orderId } = order;
-        await this.shipStationProxy.deleteOrder(orderId);
-      }),
-    );
+    try {
+      const transactionDetails = await this.authnetService.getTransactionsDetails(
+        payload.payload.id,
+      );
+      const orderNumber = transactionDetails.order.invoiceNumber;
+      await this.shipStationProxy.init();
+      const orders = await this.shipStationProxy.getListOrders({
+        orderNumber: `${orderNumber}`,
+        pageSize: `500`, // to avoid paging issues
+      });
+      await Promise.all(
+        orders.map(async (order) => {
+          const { orderId } = order;
+          await this.shipStationProxy.deleteOrder(orderId);
+        }),
+      );
+    } catch (e) {
+      Logger.error(e);
+    }
   }
 }
