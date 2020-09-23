8b81670cdc8ed50eba9888066017ad5ab34132df
Russkih Anatoly <titanofdarkness@gmail.com>
2020-09-23 19:11:34 +1000

    Minr.

--- test/bigcommercehook.e2e-spec.ts
+++ test/bigcommercehook.e2e-spec.ts
@@ -44,4 +44,25 @@ describe('BigCommerceHook (e2e)', () => {
       .expect(200)
       .expect({ status: 'WebHook was successfully processed' });
   });
+  it('/bigcomhook delete (POST)', () => {
+    const payload: WebhookUpdatedDto = {
+      created_at: 1599828823,
+      store_id: '1000249567',
+      producer: 'stores/jkfuhlnu8d',
+      scope: 'store/order/statusUpdated',
+      hash: 'da032557d2ae314c3e698a051d9792b43965e3b9',
+      data: {
+        type: 'order',
+        id: 36081, // 35797, // 35791, // 35626 - no transactionId,
+        refund: {
+          refund_id: 3,
+        },
+      },
+    };
+    return request(app.getHttpServer())
+      .post('/bigcomhook')
+      .send(payload)
+      .expect(200)
+      .expect({ status: 'WebHook was successfully processed' });
+  });
 });
